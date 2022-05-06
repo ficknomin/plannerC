73419b350317391e42ebc17f3fb5013382363665
Nick Fomin <hellkera0900@gmail.com>
2022-05-06 17:37:57 +0100

    created an event structure

---
+++ planner.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct event
+{
+    char* name;
+    int hour;
+    int minute;
+    int duration;
+    int priority;
+}event;
+
+int main()
+{
+    
+}
